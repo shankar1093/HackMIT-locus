22c03a68200c28fe1f31173bd82c7e5c253ea776
Shankar Rao <shankar1093@gmail.com>
2013-11-18 01:10:18 -0500

    Python scripts for pebble connectivity with Mac

---
+++ python-mindwave/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from distutils.core import setup
+
+setup(name="pymindwave",
+    version='1.0',
+    description='python library to interface with Neurosky Mindwave haedset',
+    author='Andreas Klostermann',
+    url='https://github.com/akloster/python-mindwave',
+    packages=['pymindwave'],
+    scripts=['bin/feedback.py', 'bin/sdl_viewer.py'],
+    )
